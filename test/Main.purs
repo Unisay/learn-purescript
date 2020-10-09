08aada1c6184f0190298a375b201caa22c267910
Yuriy Lazarev <unisay@users.noreply.github.com>
2020-10-09 18:08:38 +0200

    Demo

--- test/Main.purs
+++ test/Main.purs
@@ -1,11 +1,10 @@
 module Test.Main where
 
 import Prelude
-
 import Effect (Effect)
 import Effect.Class.Console (log)
+import Main as MMM
 
 main :: Effect Unit
 main = do
-  log "🍝"
-  log "You should add some tests."
+  log (show MMM.add)

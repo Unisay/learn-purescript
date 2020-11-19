e48868f6dc53406c578f7944be8890eb50336c63
Yuriy Lazarev <unisay@users.noreply.github.com>
2020-11-19 20:57:18 +0100

    do homework

--- src/Homework.purs
+++ src/Homework.purs
@@ -1,10 +1,10 @@
 module Homework where
 
-import Prelude
-import Data.Tuple (Tuple)
-import Partial.Unsafe (unsafeCrashWith)
-import Data.String.CodePoints (codePointFromChar)
+import Prelude (($), (<>), (>=), (>>>))
 import Data.String as String
+import Data.String.CodePoints (codePointFromChar)
+import Data.Tuple (Tuple(..), swap)
+import Partial.Unsafe (unsafeCrashWith)
 
 -- Syntactic shugar
 type T
@@ -18,22 +18,28 @@ type T
 -- Can test it in REPL with: 
 -- mapFst gt5 (Tuple 42 'x')
 mapFst :: forall a b c. (a -> c) -> T a b -> T c b
-mapFst _ = todo "mapFst"
+mapFst f (Tuple a b) = Tuple (f a) b
+
+mapFst' :: forall a b c. (a -> c) -> T a b -> T c b
+mapFst' f (Tuple a b) = swap (mapSnd f (swap (Tuple a b)))
 
 -- | Try to implement in terms of mapFst + something we learned before
 mapSnd :: forall a b c. (b -> c) -> T a b -> T a c
-mapSnd _ = todo "mapSnd"
+mapSnd f (Tuple a b) = Tuple a $ f b
+
+mapSnd' :: forall a b c. (b -> c) -> T a b -> T a c
+mapSnd' f (Tuple a b) = swap (mapFst' f (swap (Tuple a b)))
 
 -- | Given two functions, `bimap` applies them to tuple components correspondingly.
 -- Can test it in REPL with: 
 -- bimap gt5 c2s (Tuple 42 'x')
 bimap :: forall a b c d. (a -> b) -> (c -> d) -> T a c -> T b d
-bimap _ _ = todo "bimap"
+bimap f g (Tuple a c) = mapSnd g (mapFst f (Tuple a c))
 
 -- | Given two functions such that both expect argument of the same type,
 -- applies both of them to the same argument and joins reulsts in a tuple.
 fanOut :: forall a b c. (a -> b) -> (a -> c) -> a -> T b c
-fanOut _ _ = todo "fanIn"
+fanOut f g a = mapSnd g (mapFst f (Tuple a a))
 
 -- Utilities:
 todo :: forall a. String -> a

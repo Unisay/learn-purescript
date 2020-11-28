17763afe28045ae45a1094bab6160670297a4b96
wlad1CK <dkflt.b12@gmail.com>
2020-11-28 13:28:53 +0200

    added  homework

--- src/Homework.purs
+++ src/Homework.purs
@@ -1,7 +1,7 @@
 module Homework where
 
 import Prelude
-import Data.Tuple (Tuple)
+import Data.Tuple
 import Partial.Unsafe (unsafeCrashWith)
 import Data.String.CodePoints (codePointFromChar)
 import Data.String as String
@@ -18,22 +18,31 @@ type T
 -- Can test it in REPL with: 
 -- mapFst gt5 (Tuple 42 'x')
 mapFst :: forall a b c. (a -> c) -> T a b -> T c b
-mapFst _ = todo "mapFst"
+mapFst f (Tuple x y) = Tuple (f x) y
+
+mapFst' :: forall a b c. (a -> c) -> T a b -> T c b
+mapFst' f (Tuple a b) = swap (mapSnd f (swap (Tuple a b)))
 
 -- | Try to implement in terms of mapFst + something we learned before
 mapSnd :: forall a b c. (b -> c) -> T a b -> T a c
-mapSnd _ = todo "mapSnd"
+mapSnd f (Tuple x y) = Tuple x (f y)
+
+mapSnd' :: forall a b c. (b -> c) -> T a b -> T a c
+mapSnd' f (Tuple a b) = swap (mapFst f (swap (Tuple a b)))
 
 -- | Given two functions, `bimap` applies them to tuple components correspondingly.
 -- Can test it in REPL with: 
 -- bimap gt5 c2s (Tuple 42 'x')
 bimap :: forall a b c d. (a -> b) -> (c -> d) -> T a c -> T b d
-bimap _ _ = todo "bimap"
+bimap f g (Tuple a c) = Tuple (f a) (g c)
+
+bimap' :: forall a b c d. (a -> b) -> (c -> d) -> T a c -> T b d
+bimap' f g (Tuple a c) = (mapSnd' g (mapFst' f (Tuple a c)))
 
 -- | Given two functions such that both expect argument of the same type,
 -- applies both of them to the same argument and joins reulsts in a tuple.
 fanOut :: forall a b c. (a -> b) -> (a -> c) -> a -> T b c
-fanOut _ _ = todo "fanIn"
+fanOut f g a = (mapSnd' g (mapFst' f (Tuple a a)))
 
 -- Utilities:
 todo :: forall a. String -> a

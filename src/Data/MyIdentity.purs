640023b77b525d70327006749b79f813b69a17fb
Yuriy Lazarev <unisay@users.noreply.github.com>
2021-01-03 15:38:29 +0100

    Foldable instances

--- src/Data/MyIdentity.purs
+++ src/Data/MyIdentity.purs
@@ -1,8 +1,18 @@
 module Data.MyIdentity where
 
+import Prelude
+import Data.Foldable (class Foldable)
 import Data.Generic.Rep (class Generic)
 
-data MyIdentity a
+newtype MyIdentity a
   = MyIdentity a
 
 derive instance genericMyIdentity :: Generic (MyIdentity a) _
+
+instance foldableMyMaybe :: Foldable MyIdentity where
+  foldr :: forall a b. (a -> b -> b) -> b -> MyIdentity a -> b
+  foldr f b (MyIdentity a) = f a b
+  foldl :: forall a b. (b -> a -> b) -> b -> MyIdentity a -> b
+  foldl f b (MyIdentity a) = f b a
+  foldMap :: forall a m. Monoid m => (a -> m) -> MyIdentity a -> m
+  foldMap f (MyIdentity a) = f a

f2a5f8a6252737c8428f53cab4b7c3e5311c5e61
Yura <unisay@users.noreply.github.com>
2021-01-30 23:56:59 +0100

    Applicativity

--- src/Applicative.purs
+++ src/Applicative.purs
@@ -1,9 +1,12 @@
-module Apply where
+module Applicative where
 
+import Data.Unit
 import Control.Apply (class Functor, map)
 import Data.BooleanAlgebra (not)
-import Data.Maybe (Maybe(..))
+-- import Data.Maybe (Maybe(..))
 import Data.Semiring (add)
+import Data.Tuple (Tuple)
+import Homework.Todo (todo)
 
 invert :: forall f. Functor f => f Boolean -> f Boolean
 invert = map not
@@ -16,18 +19,16 @@ class
   Apply f <= Applicative f where
   pure :: forall a. a -> f a
 
-instance applyMaybe :: Apply Maybe where
-  apply :: forall c d. Maybe (c -> d) -> Maybe c -> Maybe d
-  apply mcd mc = case mcd of
-    Nothing -> Nothing
-    Just cd -> case mc of
-      Nothing -> Nothing
-      Just c -> pure (cd c)
-
-instance mbApplicative :: Applicative Maybe where
-  pure :: forall a. a -> Maybe a
-  pure = Just
-
+-- instance applyMaybe :: Apply Maybe where
+--   apply :: forall c d. Maybe (c -> d) -> Maybe c -> Maybe d
+--   apply mcd mc = case mcd of
+--     Nothing -> Nothing
+--     Just cd -> case mc of
+--       Nothing -> Nothing
+--       Just c -> pure (cd c)
+-- instance mbApplicative :: Applicative Maybe where
+--   pure :: forall a. a -> Maybe a
+--   pure = Just
 apply2 :: forall a b c f. Apply f => f (a -> b -> c) -> f a -> f b -> f c
 apply2 abcd fa fb = apply (apply abcd fa) fb
 
@@ -54,3 +55,18 @@ forall f.                                     ? ничего, просто от�
 -}
 addF :: forall f. Apply f => f Int -> f Int -> f Int
 addF f1 f2 = add `map` f1 `apply` f2
+
+class
+  Functor f <= Monoidal f where
+  munit :: f Unit
+  mapply :: forall a b. f a -> f b -> f (Tuple a b)
+
+instance applicativeMonoidal :: Applicative f => Monoidal f where
+  munit = todo "Implement using Applicative functions (map | pure | apply)"
+  mapply _ _ = todo "Implement using Applicative functions (map | pure | apply)"
+
+instance monoidalApply :: Monoidal f => Apply f where
+  apply _ _ = todo "Implement using Monoidal functions (map | munit | mapply)"
+
+instance monoidalApplicative :: Monoidal f => Applicative f where
+  pure = todo "Implement using Monoidal functions (map | munit | mapply)"

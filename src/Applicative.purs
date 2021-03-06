da132647040210488417f613bd886333f4df60e1
Yuriy Lazarev <unisay@users.noreply.github.com>
2021-03-06 21:14:34 +0100

    User parser

--- src/Applicative.purs
+++ src/Applicative.purs
@@ -1,23 +1,10 @@
 module Applicative where
 
-import Data.Unit
 import Prelude
-import Control.Apply (class Functor, map)
-import Data.BooleanAlgebra (not)
-import Data.Semiring (add)
 import Data.Tuple (Tuple(..))
-import Homework.Todo (todo)
 
 invert :: forall f. Functor f => f Boolean -> f Boolean
 invert = map not
-
-class
-  Functor f <= Apply f where
-  apply :: forall a b. f (a -> b) -> f a -> f b
-
-class
-  Apply f <= Applicative f where
-  pure :: forall a. a -> f a
 
 -- instance applyMaybe :: Apply Maybe where
 --   apply :: forall c d. Maybe (c -> d) -> Maybe c -> Maybe d
@@ -36,7 +23,7 @@ lift2 :: forall a b c f. Applicative f => (a -> b -> c) -> f a -> f b -> f c
 lift2 abcd fa fb = apply (map abcd fa) fb
 
 apply3 :: forall a b c d f. Apply f => f (a -> b -> c -> d) -> f a -> f b -> f c -> f d
-apply3 abcd fa fb fc = apply (apply (apply abcd fa) fb) fc
+apply3 abcd fa fb fc = abcd <*> fa <*> fb <*> fc
 
 lift3 :: forall a b c d f. Applicative f => (a -> b -> c -> d) -> f a -> f b -> f c -> f d
 lift3 abcd fa fb fc = apply (apply (map abcd fa) fb) fc
@@ -54,7 +41,7 @@ forall f.                                     ? ничего, просто от�
       forall (f :: Type -> Type). Apply f =>  ? + делать apply2,3,4,....
 -}
 addF :: forall f. Apply f => f Int -> f Int -> f Int
-addF f1 f2 = add `map` f1 `apply` f2
+addF f1 f2 = add `map` f1 <*> f2
 
 class
   Functor f <= Monoidal f where
@@ -65,10 +52,9 @@ instance applicativeMonoidal :: Applicative f => Monoidal f where
   munit = pure unit
   mapply = lift2 Tuple
 
-instance monoidalApply :: Monoidal f => Apply f where
-  apply :: forall x y. f (x -> y) -> f x -> f y
-  apply fxy fx = map (\(Tuple xy x) -> xy x) (mapply fxy fx)
-
-instance monoidalApplicative :: Monoidal f => Applicative f where
-  pure :: forall a. a -> f a
-  pure a = map (const a) munit
+-- instance monoidalApply :: Monoidal f => Apply f where
+--   apply :: forall x y. f (x -> y) -> f x -> f y
+--   apply fxy fx = map (\(Tuple xy x) -> xy x) (mapply fxy fx)
+-- instance monoidalApplicative :: Monoidal f => Applicative f where
+--   pure :: forall a. a -> f a
+--   pure a = map (const a) munit

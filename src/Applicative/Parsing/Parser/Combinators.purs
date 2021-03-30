dfeb2b349f7d26062df85d4f3c9ca3b8fcf0d736
Yuriy Lazarev <unisay@users.noreply.github.com>
2021-03-30 16:42:36 +0200

    unfinished sequence combinator

--- src/Applicative/Parsing/Parser/Combinators.purs
+++ src/Applicative/Parsing/Parser/Combinators.purs
@@ -1,1 +1,9 @@
 module Applicative.Parsing.Parser.Combinators where
+
+import Applicative.Parsing.Types
+import Prelude
+import Control.Apply (lift2)
+import Data.Array (foldr, (:))
+
+sequence :: forall a. Array (Parser a) -> Parser (Array a)
+sequence = foldr (?acc) (pure [])

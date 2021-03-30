e66324c1bfca199a6a172a5c643ba757306524ff
Yuriy Lazarev <unisay@users.noreply.github.com>
2021-03-30 16:48:07 +0200

    Homework

--- src/Applicative/Parsing/Parser/Combinators.purs
+++ src/Applicative/Parsing/Parser/Combinators.purs
@@ -5,5 +5,15 @@ import Prelude
 import Control.Apply (lift2)
 import Data.Array (foldr, (:))
 
+-- Homework:
+-- 1. Finish implementation (replace typed hole)
+-- 2. Make function type more general (polymorphic)
 sequence :: forall a. Array (Parser a) -> Parser (Array a)
 sequence = foldr (?acc) (pure [])
+
+-- Homework: 
+-- 1. implement traverse directly
+-- 2. implement traverse via sequence
+-- 3. implement sequence via traverse
+traverse :: forall a b. (a -> b) -> Array (Parser a) -> Parser (Array b)
+traverse = ?implement

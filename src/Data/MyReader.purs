f337192d3aa33182e510222450f8227e1cd96150
emonti <39101979+tryuhan@users.noreply.github.com>
2021-04-21 12:20:28 +0300

    Do homework
    Co-authored-by: Yuriy Lazarev <Unisay@users.noreply.github.com>
    Co-authored-by: Vadym <VadymVadymov@users.noreply.github.com>

--- src/Data/MyReader.purs
+++ src/Data/MyReader.purs
@@ -1,18 +1,68 @@
 module Data.MyReader where
 
 import Prelude
-import Class.MyFunctor (class MyFunctor)
 import Data.Generic.Rep (class Generic)
-import Helper (notImplemented)
+import Data.Maybe
 
 newtype MyReader r a
   = MyReader (r -> a)
 
-instance myFunctor :: MyFunctor (MyReader r) where
-  fmap :: forall a b. (a -> b) -> MyReader r a -> MyReader r b
-  fmap f (MyReader g) = MyReader (f <<< g)
+instance functorMyReader :: Functor (MyReader r) where
+  map :: forall a b. (a -> b) -> MyReader r a -> MyReader r b
+  map f (MyReader g) = MyReader (f <<< g)
+
+instance applyMyReader :: Apply (MyReader r) where
+  apply :: forall a b. MyReader r (a -> b) -> MyReader r a -> MyReader r b
+  apply (MyReader rab) (MyReader ra) = MyReader $ apply rab ra
+
+instance applicativeMyReader :: Applicative (MyReader r) where
+  pure = MyReader <<< pure
+
+instance bindMyReader :: Bind (MyReader r) where
+  bind (MyReader ra) f = MyReader \r -> let MyReader rb = f (ra r) in rb r
 
 runMyReader :: forall r a. r -> MyReader r a -> a
-runMyReader = notImplemented
+runMyReader r (MyReader f) = f r
+
+ask :: forall r. MyReader r r
+ask = MyReader identity
 
 derive instance genericMyReader :: Generic (MyReader r a) _
+
+type Age
+  = Int
+
+type Beer
+  = String
+
+type Girls
+  = String
+
+type Fun
+  = String
+
+buyBeer :: MyReader Age (Maybe Beer)
+buyBeer = ask <#> \age -> if age >= 18 then Just "Porter" else Nothing
+
+pickGirls :: MyReader Age (Maybe Girls)
+pickGirls = ado
+  age <- ask
+  in if age >= 21 then Just "Real WOW" else Nothing
+
+makeFun :: MyReader Age (Beer -> Girls -> Fun)
+makeFun =
+  ask
+    <#> \age beer girls -> if age > 23 then "Total Fun!" else "No Fun :("
+
+makeSuperParty :: MyReader Age (Maybe Fun)
+makeSuperParty = do -- MyReader Age a
+  maybeBeer <- buyBeer
+  maybeGirls <- pickGirls
+  f <- makeFun
+  pure do -- Maybe a
+    beer <- maybeBeer
+    girls <- maybeGirls
+    pure (f beer girls)
+
+type MyReaderMaybe r a
+  = MyReader r (Maybe a)

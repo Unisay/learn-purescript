967452b65d5f107699743a4b1f7c5d68d1998baf
Yuriy Lazarev <unisay@users.noreply.github.com>
2021-08-03 09:42:00 +0200

    countScalars is not tail-recursive (yet)

--- src/JSON.js
+++ src/JSON.js
@@ -10,5 +10,9 @@ exports.sampleJson1 = {
   mapping: {
     addressLines: ["Andreasstr.", "10243", "8 stock"],
     1: 2,
+    nested: { els: [1] },
   },
 };
+
+exports.sampleArray10 = [[[[[1]]]]];
+exports.sampleHomoObject = { key: "value", key2: "value2" };
--- src/JSON.purs
+++ src/JSON.purs
@@ -5,16 +5,22 @@ import Data.Argonaut (Json)
 import Data.Argonaut as A
 import Data.Argonaut.Parser (jsonParser)
 import Data.Either (Either)
+import Data.Array as Array
+import Data.Foldable (sum)
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Class.Console (log)
+import Foreign.Object (Object)
 import Foreign.Object as Object
-import Helper (notImplemented)
 
 foreign import sampleJson1 :: Json
 
+foreign import sampleArray10 :: Json
+
 someNumber :: Json
 someNumber = A.fromNumber 23.6
+
+foreign import sampleHomoObject :: Object String
 
 someObject :: Json
 someObject =
@@ -63,7 +69,29 @@ jsonType =
     (const "object")
 
 -- Counts scalar json values recursively,
---  e.g. [1, true, { "name": "ok" }] = 3
+-- e.g. [1, true, { "name": "ok" }] = 3
 -- countScalars sampleJson1 = 12
 countScalars :: Json -> Int
-countScalars = notImplemented
+countScalars =
+  A.caseJson
+    (const 1)
+    (const 1)
+    (const 1)
+    (const 1)
+    (\a -> countArray a)
+    (\o -> countArray (Object.values o))
+  where
+  countArray = Array.foldr (\el a -> a + countScalars el) 0
+
+-- | ```purescript
+-- | compactJson (nest 0) == []
+-- | compactJson (nest 1) == [[]]
+-- | compactJson (nest 2) == [[[]]]
+-- | compactJson (nest 3) == [[[[]]]]
+-- | ```
+nest :: Int -> Json
+nest iter = (go { iter, json: A.jsonEmptyArray }).json
+  where
+  go a
+    | a.iter < 1 = a
+    | otherwise = go { iter: a.iter - 1, json: A.jsonSingletonArray a.json }

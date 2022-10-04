e08abac16969ad15635dbb88ce68a268a8c9aa07
VadymVadymov <vadymvadymov@users.noreply.github.com>
2022-10-04 18:45:22 +0200

    feat: an attempt to create recordPlayer

--- src/FSM.purs
+++ src/FSM.purs
@@ -2,7 +2,14 @@ module FSM where
 
 import Custom.Prelude
 
+import Control.Monad.Cont (class MonadTrans, lift)
+import Control.Monad.State (State)
+import Control.Monad.State.Class (class MonadState)
+import Control.Monad.State.Trans (get, state)
 import Data.Array as Array
+import Data.Array.NonEmpty (NonEmptyArray)
+import Data.Array.NonEmpty as NEA
+import Data.Bifunctor (lmap)
 import Data.Foldable (foldl)
 import Data.Identity (Identity)
 import Data.Newtype (class Newtype, unwrap)
@@ -16,8 +23,18 @@ newtype FSMT i m o = FSMT (i → (Tuple (m o) (FSMT i m o)))
 
 derive instance Newtype (FSMT i m o) _
 
+instance MonadTrans (FSMT i) where
+  lift ∷ ∀ m a i. Monad m ⇒ m a → FSMT i m a
+  lift m = ?x
+
+instance MonadState s m ⇒ MonadState s (FSMT i m) where
+  state f = lift (state f)
+
 stepFSMT ∷ ∀ i m o. FSMT i m o → i → Tuple (m o) (FSMT i m o)
 stepFSMT = unwrap
+
+stepFSM ∷ ∀ i o. FSM i o → i → Tuple o (FSM i o)
+stepFSM fsm = unwrap fsm >>> lmap unwrap
 
 type FSM i o = FSMT i Identity o
 
@@ -62,3 +79,30 @@ test = do
   pure $ outputs == [ Red, Red, Green, Green, Red, Red ]
 
 -- ⏪⏩▶⏸⏹
+
+type RecordPlayer = FSMT RPInput (State RPState) RPOutput
+
+data RPInput = SwitchForward | SwitchBack | OnOffButton | Pause | StopPlay
+
+type RPState =
+  { nowPlaying ∷ Maybe Song, isTurnedOn ∷ Boolean, playlist ∷ Playlist }
+
+type RPOutput = { nowPlaying ∷ Maybe Song, isTurnedOn ∷ Boolean }
+
+type Song = { author ∷ String, songName ∷ String }
+
+type Playlist = NonEmptyArray Song
+
+recordPlayerOn ∷ RecordPlayer
+recordPlayerOn = FSMT case _ of
+  SwitchForward → do
+    { nowPlaying, playlist } ← get
+    case nowPlaying of
+      Nothing → Tuple (pure { nowPlaying: Nothing, isTurnedOn: true })
+        recordPlayerOn
+      Just song → Tuple
+        ( pure
+            { nowPlaying: fromMaybe (NEA.head playlist) $ NEA.index playlist
+                (add 1 $ fromMaybe 0 $ NEA.elemIndex song playlist)
+            }
+        )

2cb974a2f483b46115090c834367d0e20563667f
VadymVadymov <vadymvadymov@users.noreply.github.com>
2022-10-07 17:25:42 +0200

    feat: `FSMT` is monad && `recordPlayer` implement

--- src/FSM.purs
+++ src/FSM.purs
@@ -1,22 +1,23 @@
 module FSM where
 
-import Custom.Prelude
 import Prelude
 
 import Control.Monad.Cont (class MonadTrans, lift)
 import Control.Monad.State (State)
 import Control.Monad.State.Class (class MonadState)
-import Control.Monad.State.Trans (get, state)
+import Control.Monad.State.Trans (gets, state)
+import Custom.Prelude (Maybe(..))
 import Data.Array as Array
 import Data.Array.NonEmpty (NonEmptyArray)
 import Data.Array.NonEmpty as NEA
-import Data.Bifunctor (lmap)
+import Data.Bifunctor (bimap, lmap)
 import Data.Foldable (foldl)
 import Data.Identity (Identity)
 import Data.Machine.Mealy (MealyT, Step(..), fromArray, mealy, toUnfoldable)
 import Data.Newtype (class Newtype, unwrap)
 import Data.NonEmpty (NonEmpty, (:|))
 import Data.Tuple (Tuple(..))
+import Data.Tuple as Tuple
 import Effect (Effect)
 import Effect.Class.Console (log)
 
@@ -40,11 +41,23 @@ instance MonadTrans (FSMT i) where
 
 -}
 
-instance Functor (FSMT i m)
-instance Apply (FSMT i m)
-instance Applicative (FSMT i m)
-instance Bind (FSMT i m)
-instance Monad (FSMT i m)
+instance Functor m ⇒ Functor (FSMT i m) where
+  map f (FSMT v) = FSMT \i → bimap (map f) (map f) (v i)
+
+instance Monad m ⇒ Apply (FSMT i m) where
+  apply = ap
+
+instance Monad m ⇒ Applicative (FSMT i m) where
+  pure a = FSMT $ const $ Tuple (pure a) (pure a)
+
+instance Monad m ⇒ Bind (FSMT i m) where
+
+  bind (FSMT v) f = FSMT \i → Tuple (m' i) (fsmt' i)
+    where
+    m' i = Tuple.fst (v i) >>= \a → let (FSMT v') = f a in Tuple.fst $ v' i
+    fsmt' i = Tuple.snd (v i) >>= \a → let (FSMT v') = f a in Tuple.snd $ v' i
+
+instance Monad m ⇒ Monad (FSMT i m)
 
 instance MonadState s m ⇒ MonadState s (FSMT i m) where
   state f = lift (state f)
@@ -105,29 +118,79 @@ data RPInput = Forward | Rewind | Play | Pause | Stop
 
 type RPState =
   { nowPlaying ∷ Maybe Song
-  , isTurnedOn ∷ Boolean
   , playlist ∷ Playlist
   }
 
-type RPOutput = { nowPlaying ∷ Maybe Song, isTurnedOn ∷ Boolean }
+type RPOutput = { nowPlaying ∷ Maybe Song, isPaused ∷ Boolean }
 
 type Song = { author ∷ String, songName ∷ String }
 
 type Playlist = NonEmptyArray Song
 
-recordPlayerOn ∷ RecordPlayer
-recordPlayerOn = FSMT case _ of
-  Forward → do
-    { nowPlaying, playlist } ← get
-    case nowPlaying of
-      Nothing → Tuple (pure { nowPlaying: Nothing, isTurnedOn: true })
-        recordPlayerOn
-      Just song → Tuple
-        ( pure
-            { nowPlaying: fromMaybe (NEA.head playlist) $ NEA.index playlist
-                (add 1 $ fromMaybe 0 $ NEA.elemIndex song playlist)
-            }
-        )
+recordPlayerPlaying ∷ RecordPlayer
+recordPlayerPlaying = FSMT case _ of
+  Forward → flip Tuple recordPlayerPlaying $
+    gets \{ nowPlaying, playlist } →
+      { isPaused: false
+      , nowPlaying: do
+          s ← nowPlaying
+          idx ← NEA.elemIndex s playlist
+          if idx == NEA.length playlist - 1 then Just (NEA.head playlist)
+          else NEA.index playlist (idx + 1)
+      }
+
+  Rewind → flip Tuple recordPlayerPlaying $
+    gets \{ nowPlaying, playlist } →
+      { isPaused: false
+      , nowPlaying: do
+          s ← nowPlaying
+          idx ← NEA.elemIndex s playlist
+          if idx == 0 then Just (NEA.last playlist)
+          else NEA.index playlist (idx - 1)
+      }
+
+  Play → Tuple
+    (gets \{ nowPlaying } → { nowPlaying: nowPlaying, isPaused: false })
+    recordPlayerPlaying
+
+  Pause → flip Tuple recordPlayerPaused $
+    gets \{ nowPlaying } → { nowPlaying: nowPlaying, isPaused: true }
+
+  Stop → flip Tuple recordPlayerPaused $ pure
+    { nowPlaying: Nothing, isPaused: true }
+
+recordPlayerPaused ∷ RecordPlayer
+recordPlayerPaused = FSMT case _ of
+  Forward → flip Tuple recordPlayerPlaying $
+    gets \{ nowPlaying, playlist } →
+      { isPaused: true
+      , nowPlaying: do
+          s ← nowPlaying
+          idx ← NEA.elemIndex s playlist
+          if idx == NEA.length playlist - 1 then Just (NEA.head playlist)
+          else NEA.index playlist (idx + 1)
+      }
+
+  Rewind → flip Tuple recordPlayerPlaying $
+    gets \{ nowPlaying, playlist } →
+      { isPaused: true
+      , nowPlaying: do
+          s ← nowPlaying
+          idx ← NEA.elemIndex s playlist
+          if idx == 0 then Just (NEA.last playlist)
+          else NEA.index playlist (idx - 1)
+      }
+
+  Play → flip Tuple recordPlayerPlaying $
+    gets \{ nowPlaying, playlist } → case nowPlaying of
+      Nothing → { nowPlaying: Just $ NEA.head playlist, isPaused: false }
+      song → { nowPlaying: song, isPaused: false }
+
+  Pause → Tuple
+    (gets \{ nowPlaying } → { nowPlaying: nowPlaying, isPaused: true })
+    recordPlayerPaused
+
+  Stop → Tuple (pure { nowPlaying: Nothing, isPaused: true }) recordPlayerPaused
 
 type M i o = MealyT Identity i o
 
